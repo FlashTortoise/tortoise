0dce83087f714c5f87a1bb0b69a738179de2a083
xloc <tj-xzx@126.com>
2017-03-12 11:20:02 +0800

    Try to use config of flask

--- tortoise.py
+++ tortoise.py
@@ -1,8 +1,11 @@
+import pkgutil
 import time
 from contextlib import contextmanager
+import sys, os
 
 from globals import ctx
 from sensors import cache_input
+from config import Config
 
 PERIOD = 0.1
 
@@ -28,9 +31,32 @@ def in_a_period():
         raise Exception('running time exceed period')
 
 
+def get_root_path(import_name):
+    """Returns the path to a package or cwd if that cannot be found.  This
+    returns the path of a package or the folder that contains a module.
+
+    Not to be confused with the package path returned by :func:`find_package`.
+    """
+    # Module already imported and has a file attribute.  Use that first.
+    mod = sys.modules.get(import_name)
+    if mod is not None and hasattr(mod, '__file__'):
+        return os.path.dirname(os.path.abspath(mod.__file__))
+
+    # Next attempt: check the loader.
+    loader = pkgutil.get_loader(import_name)
+
+    # Loader does not exist or we're referring to an unloaded main module
+    # or a main module without path (interactive sessions), go with the
+    # current working directory.
+    if loader is None or import_name == '__main__':
+        return os.getcwd()
+
+
 class Tortoise(object):
-    def __init__(self):
+    def __init__(self, import_name):
         self.task = None
+        root_path = get_root_path(import_name)
+        ctx.config = Config(root_path)
         pass
 
     def walk(self):
