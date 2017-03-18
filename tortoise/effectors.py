688987910bf556ef969a66bb56bd3b3ec07591a4
xloc <tj-xzx@126.com>
2017-03-18 20:20:40 +0800

    Use a onservative and correct way to assign

--- tortoise/effectors.py
+++ tortoise/effectors.py
@@ -6,14 +6,10 @@ class Wheels(object):
         # noinspection PyUnresolvedReferences
         from gpiozero import Motor
         self.Motor = Motor
-        self.mq, self.mw, self.ma, self.ms = map(
-            Motor, [
-                config.WHEELS_PINS_LF,
-                config.WHEELS_PINS_LB,
-                config.WHEELS_PINS_RF,
-                config.WHEELS_PINS_RB,
-            ]
-        )
+        self.mq = Motor(*config.WHEELS_PINS_LF)
+        self.ma = Motor(*config.WHEELS_PINS_LB)
+        self.mw = Motor(*config.WHEELS_PINS_RF)
+        self.ms = Motor(*config.WHEELS_PINS_RB)
 
     def set(self, l, r):
         self.set_lr(l, r)
@@ -25,6 +21,7 @@ class Wheels(object):
         self.set_lr(speed-diff, speed+diff)
 
     def set_raw(self, q, a, w, s):
-        map(self.Motor.value,
-            (self.mq, self.ma, self.mw, self.ms),
-            (q, a, w, s))
+        self.mq.value = q
+        self.ma.value = a
+        self.mw.value = w
+        self.ms.value = s
