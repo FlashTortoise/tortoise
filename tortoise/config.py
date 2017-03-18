8b34cbba9e707fe06cf0105590e6f192910a4849
xloc <tj-xzx@126.com>
2017-03-18 20:25:35 +0800

    Fill in pin maps for wheel motors

--- tortoise/config.py
+++ tortoise/config.py
@@ -2,11 +2,12 @@ EYE_CAPTURE_ID = 0
 EYE_SIGHT_HEIGHT = 640
 EYE_SIGHT_WIDTH = 320
 
-WHEELS_PINS_LF = ()
-WHEELS_PINS_LB = ()
-WHEELS_PINS_RF = ()
-WHEELS_PINS_RB = ()
+WHEELS_PINS_LF = (5, 13)
+WHEELS_PINS_LB = (19, 26)
+WHEELS_PINS_RF = (12, 16)
+WHEELS_PINS_RB = (20, 21)
 
 TORTOISE_WALK_PERIOD = 0.1
 
-RECORDER_PATH = '~/tortoise/'
+# fix it: it seems that only relative directory can by accessed by open
+RECORDER_PATH = '.'
