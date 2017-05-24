1ea0003620219fd5d665f5e51a2b0c1f9b1a6350
xloc <tj-xzx@126.com>
2017-05-24 21:30:27 +0800

    Add test for all wheels combinations

---
+++ test/test_remoteWheels_all_combination.py
@@ -0,0 +1,35 @@
+import time
+
+import itertools
+
+from tortoise.effectors import RemoteWheels
+
+w = RemoteWheels()
+
+
+def all_combinations(num=4):
+    for i in range(num):
+        print 'select %d: ' % (i + 1)
+        for j in itertools.combinations(range(4), i + 1):
+            speeds = [0] * 4
+            for ii in j:
+                speeds[ii] = 1
+            yield speeds
+
+
+for speeds in all_combinations():
+    print speeds
+
+    vs = [v * 0.2 for v in speeds]
+    print vs
+    w.set_raw(*vs)
+    # time.sleep(1)
+    raw_input('pause')
+
+    # vs = [v * -0.2 for v in speeds]
+    # print vs
+    # w.set_raw( *vs )
+    # # time.sleep(1)
+    # raw_input('pause')
+
+w.set_raw(*([0] * 4))
