d41b1ea265b509307f5146cb82d5a57a31cd66cd
xloc <tj-xzx@126.com>
2017-05-25 21:05:58 +0800

    Catch KeyboardInterrupt for stop

--- test/test_remoteWheels.py
+++ test/test_remoteWheels.py
@@ -4,19 +4,20 @@ from tortoise.effectors import RemoteWheels
 
 w = RemoteWheels()
 
-for i in range(4):
-    speeds = [0] * 4
+try:
+    for i in range(4):
+        speeds = [0] * 4
 
-    print "wheel {} is setting to 1".format(i)
-    speeds[i] = 0.2
-    w.set_raw(*speeds)
-    # time.sleep(1)
-    raw_input('pause')
+        print "wheel {} is setting to 1".format(i)
+        speeds[i] = 0.2
+        w.set_raw(*speeds)
+        # time.sleep(1)
+        raw_input('pause')
 
-    print "wheel {} is setting to -1".format(i)
-    speeds[i] = -0.2
-    w.set_raw(*speeds)
-    # time.sleep(1)
-    raw_input('pause')
-
-w.set_raw(*([0] * 4))
+        print "wheel {} is setting to -1".format(i)
+        speeds[i] = -0.2
+        w.set_raw(*speeds)
+        # time.sleep(1)
+        raw_input('pause')
+finally:
+    w.set_raw(*([0] * 4))
