3c940eec7a3a55c17f920b6f0337895823d2f30c
xloc <tj-xzx@126.com>
2017-06-03 00:17:10 +0800

    Add available non-blocking helpers

--- tool/__init__.py
+++
@@ -1,1 +0,0 @@
-from no_block import *
--- tortoise/helper.py
+++ tortoise/helper.py
@@ -1,3 +1,8 @@
+from collections import deque
+from itertools import repeat
+from more_itertools import peekable
+
+
 """Constrain helper function"""
 
 
@@ -41,3 +46,71 @@ def closure_mlimit(m):
             return a
 
     return limit
+
+
+""" Non-blocking helpers """
+
+
+def run_n_time_flag(self, distinct_name, time=1, peek=False):
+    if getattr(self, '_execute_record', None) is None:
+        setattr(self, '_execute_record', {})
+
+    rec = getattr(self, '_execute_record')
+
+    if rec.get(distinct_name, None) is None:
+        rec[distinct_name] = time
+
+    if rec[distinct_name] > 0:
+        if peek is False:
+            rec[distinct_name] -= 1
+        return True
+    else:
+        return False
+
+
+class StepManager(object):
+    def __init__(self):
+        self.tasks = deque()
+
+    def need_step(self):
+        while True:
+            try:
+                task_info = self.tasks[0]
+            except IndexError:
+                return False
+
+            try:
+                p = task_info['need_iter'].peek()
+            except StopIteration:
+                p = False
+
+            if p is False:
+                self.tasks.popleft()
+            else:
+                return True
+
+    def add_n_times(self, step, times):
+        if not callable(step):
+            raise Exception('task is not callable')
+
+        self.tasks.append(dict(
+            callable=step,
+            need_iter=peekable(repeat(True, times))
+        ))
+
+    def add(self, step):
+        self.add_n_times(step, times=1)
+
+    def add_blocking(self, step, until):
+        self.tasks.append(dict(
+            callable=step,
+            need_iter=peekable(iter(until, None))
+        ))
+
+    def step(self):
+        need_iter = self.tasks[0]['need_iter'].next()
+        task = self.tasks[0]['callable']
+        if not need_iter:
+            self.tasks.popleft()
+
+        return task()
